d4f2901307299bd9ca664c97142855c9e8742a66
Jackzzl <1140068696@qq.com>
2018-06-09 13:07:34 +0800

    qrr

---
+++ Cppsort/cmake-build-debug/CMakeFiles/Cppsort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cppsort.dir/main.cpp.o"
+  "Cppsort.pdb"
+  "Cppsort.exe"
+  "libCppsort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cppsort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
